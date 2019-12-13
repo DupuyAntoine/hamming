92aa27acc638cfa5b990b1706a2197946fbd6c73
Thomas COLETTE <colette.thomas@outlook.com>
2019-12-13 09:17:21 +0100

    Fixed G & H generation + Encoding function

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hamming
+
+ * Deux fonctions MatriceH et MatriceG qui étant donné l calculant une matrice de parité et génératrice du code de Hamming pour l (DONE)
+ * Une fonction Encodage d'une bloc de taille 2^l-l-1
+ * Une fonction Bruitage qui altère un bit parmi 2^l-1
+ * Une fonction Décodage qui décode un mot de taille 2^l-1
+ * Une fonction Hachage qui découpe un bloc quelconque en sous blocs de taille 2^l-l-1
+ * Bonus : Pour n, k implémenter la fonction qui calcule le nombre max d'erreurs corrigibles qui provient de l'empilement des sphères
--- hamming.py
+++ hamming.py
@@ -1,3 +1,12 @@
+# coding: utf-8
+
+# Deux fonctions MatriceH et MatriceG qui étant donné l calculant une matrice de parité et génératrice du code de Hamming pour l
+# Une fonction Encodage d'une bloc de taille 2^l-l-1
+# Une fonction Bruitage qui altère un bit parmi 2^l-1
+# Une fonction Décodage qui décode un mot de taille 2^l-1
+# Une fonction Hachage qui découpe un bloc quelconque en sous blocs de taille 2^l-l-1
+# Bonus : Pour n, k implémenter la fonction qui calcule le nombre max d'erreurs corrigibles qui provient de l'empilement des sphères
+
 def dec_to_binary(nb, nbBits):
     """ Converti un nombre décimal en binaire """
     binary = []
@@ -23,6 +32,15 @@ def is_power_two(nb):
     """ Vérifie si un nombre est une puissance de 2 """
     return (nb & (nb-1) == 0) and (nb != 0)
 
+def matmul(mot, mat):
+    """ Multiplie un mot par une matrice """
+    tmp = [0] * (len(mat[0]))
+    for i in range(len(mot)):
+        for j in range(len(mat[0])):
+            tmp[j] = (tmp[j] + (mot[i] * mat[i][j])) % 2
+    return tmp
+
+
 
 class Hamming:
 
@@ -42,19 +60,24 @@ class Hamming:
                 matriceH.append(dec_to_binary(i, self.n - self.k))
         for elt in identite:
             matriceH.append(elt)
-        return matriceH
+        return transpose(matriceH)
 
     def matriceG(self, matriceH):
         """ Génère une matrice G avec une matrice H donnée """
         matriceG = buildIdentite(self.k)
         temp = []
-        for elt in matriceH:
+        for elt in transpose(matriceH):
             if sum(elt) != 1:
                 temp.append(elt)
         transposeTemp = transpose(temp)
         for elt in transposeTemp:
             matriceG.append(elt)
-        return matriceG
+        return transpose(matriceG)
+
+    def encodage(self, mot, matGen):
+        """ Encode un mot de taille 2^l - l - 1 """
+        return matmul(mot, matGen)
+
 
 
 if __name__ == '__main__':
@@ -64,3 +87,6 @@ if __name__ == '__main__':
     print('\n')
     g = hamming.matriceG(h)
     print(g)
+    m = [1, 1, 0, 0]
+    md = hamming.encodage(m, g)
+    print(md)
